a08b83092cd6a97fad32c9c834893670c23c79a4
syahrial6 <syr06rial@gmail.com>
2023-08-03 09:16:49 +0700

    baru2

--- index.js
+++ index.js
@@ -3,55 +3,58 @@ import cors from "cors";
 import session from "express-session";
 import fileUpload from "express-fileupload";
 import dotenv from "dotenv";
-import UserRoute from "./routes/UserRoute.js"
-import SiswaRoute from "./routes/SiswaRoute.js"
+import UserRoute from "./routes/UserRoute.js";
+import SiswaRoute from "./routes/SiswaRoute.js";
 import SurahRoute from "./routes/SurahRoute.js";
-import HistoryRoute from "./routes/HistoryRoute.js"
+import HistoryRoute from "./routes/HistoryRoute.js";
 import db from "./config/Database.js";
-import AuthRoute from "./routes/AuthRoute.js"
+import AuthRoute from "./routes/AuthRoute.js";
 import SequelizeStore from "connect-session-sequelize";
 
-
-
-
-
-dotenv.config()
+dotenv.config();
 
 const app = express();
 
 const session_store = SequelizeStore(session.Store);
 const store = new session_store({
-    db : db
+  db: db,
 });
-
 
 // (async() => {
 //         await db.sync();
 // })();
 
-app.use(session({
-    secret:process.env.SESSION_SECRET,
-    resave:false,
-    store:store,
-    saveUninitialized:true,
-    cookie:{
-        secure: "auto"
-    }
-}))
+// app.use(session({
+//     secret:process.env.SESSION_SECRET,
+//     resave:false,
+//     store:store,
+//     saveUninitialized:true,
+//     cookie:{
+//         secure: "auto"
+//     }
+// }))
 
-app.use(cors({
-    credentials:true,
-    origin: "http://localhost:3000"
+const oneDay = 1000 * 60 * 60 * 24;
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    saveUninitialized: true,
+    cookie: { maxAge: oneDay },
+    resave: false,
+  })
+);
 
-}))
-
-
-
-
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
 
 app.use(express.json());
-app.use(fileUpload())
-app.use(express.static("public"))
+app.use(fileUpload());
+app.use(express.static("public"));
 app.use(UserRoute);
 app.use(SiswaRoute);
 app.use(AuthRoute);
@@ -60,6 +63,6 @@ app.use(HistoryRoute);
 
 // store.sync();
 
-app.listen(process.env.APP_PORT, ()=>{
-    console.log("Server jalan")
-})
+app.listen(process.env.APP_PORT, () => {
+  console.log("Server jalan");
+});
